c3cd512e5a11bb125e8865613528670bfd1f34c4
ariana0505 <penaariana075@gmail.com>
2026-02-05 16:50:33 -0500

    fix

--- 21-house-robber/house_robber.py
+++ 21-house-robber/house_robber.py
@@ -1,12 +1,13 @@
 # rob1: máximo dinero robado hasta la casa i-2
 # rob2: máximo dinero robado hasta la casa i-1
-rob1, rob2 = 0, 0
-nums = [2,7,9,3,1]
+rob1 = 0
+rob2 = 0
+casas = [2,7,9,3,1]
 
-for n in nums:
+for dinero in casas:
     # Opción 1: robar esta casa → n + rob1
     # Opción 2: no robar esta casa → rob2
-    temp = max(n + rob1, rob2)
+    temp = max(dinero + rob1, rob2)
 
     # Desplazamos los estados para la siguiente iteración
     rob1 = rob2      # lo que era i-1 ahora será i-2
