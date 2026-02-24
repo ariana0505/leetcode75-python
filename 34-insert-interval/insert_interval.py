2adef7370c525fee22881ccbae58b55bae6b625b
ariana0505 <penaariana075@gmail.com>
2026-02-23 19:32:42 -0500

    add problem

--- 34-insert-interval/insert_interval.py
+++ 34-insert-interval/insert_interval.py
@@ -5,13 +5,13 @@ newInterval = [2, 5]
 res = []
 
 for i in range(len(intervals)):
-    if newInterval[1] < intervals[i][0]:
+    if newInterval[1] < intervals[i][0]: # no soplan, esta antes
         res.append(newInterval)
         res.extend(intervals[i:])
         break
-    elif newInterval[0] > intervals[i][1]:
+    elif newInterval[0] > intervals[i][1]: #no soplan, esta despues
         res.append(intervals[i])
-    else:
+    else: # si soplan
         newInterval = [
             min(newInterval[0], intervals[i][0]),
             max(newInterval[1], intervals[i][1])
