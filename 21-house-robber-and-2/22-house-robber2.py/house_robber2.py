6eb87f8b87438cc517735776d6b3f41abe34f2ee
ariana0505 <penaariana075@gmail.com>
2026-03-22 01:26:44 -0500

    feat: add House Robber 2 solution with English comments

---
+++ 21-house-robber-and-2/22-house-robber2.py/house_robber2.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+def rob_linear(nums: List[int]) -> int:
+    # House Robber 1: linear algorithm
+    rob1, rob2 = 0, 0
+    for money in nums:
+        temp: int = max(money + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+    return rob2
+
+
+houses: List[int] = [2, 7, 9, 3, 1]
+
+# Break the circle into two lines
+# Line 1: houses[0 ... n-2] (include first, exclude last)
+# Line 2: houses[1 ... n-1] (exclude first, include last)
+
+result: int = max(rob_linear(houses[:-1]), rob_linear(houses[1:]))
+print(result)
