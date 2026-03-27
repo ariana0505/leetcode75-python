7e402dc28612e5601223a5d58b48a1f48509fc50
ariana0505 <penaariana075@gmail.com>
2026-03-27 11:07:52 -0500

    feat: add Meeting Rooms practice using sorted interval comparison

---
+++ practice/meeting_rooms.py
@@ -0,0 +1,8 @@
+intervals = [[7,10],[2,4]]
+intervals.sort()
+for i in range(len(intervals)-1):
+    if intervals[i][1] > intervals[i + 1][0]:
+        print(False)
+        break
+else:
+    print(True)
