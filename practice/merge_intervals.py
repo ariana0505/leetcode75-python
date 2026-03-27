8872099482d419c37b0b5a0cde4396216d1ec703
ariana0505 <penaariana075@gmail.com>
2026-03-27 14:43:30 -0500

    feat: add Merge Intervals practice using sorted interval comparison

---
+++ practice/merge_intervals.py
@@ -0,0 +1,11 @@
+intervals = [[1,3],[2,6],[8,10],[15,18]]
+intervals.sort()
+result = [intervals[0]]
+
+for start, end in intervals[1:]:
+    if result[-1][1] >= start:
+        result[-1][1] = max(result[-1][1], end)
+    else:
+        result.append([start, end])
+
+print(result)
