cafa7ff34718ff961177a855e3d9e9634786e41f
ariana0505 <penaariana075@gmail.com>
2026-03-29 01:02:49 -0500

    feat: add Minimum Window Substring practice using sliding window

--- practice/minimum_window_substring.py
+++ practice/minimum_window_substring.py
@@ -5,27 +5,25 @@ t = "ABC"
 letras_t =  Counter(t)
 
 have = 0 # letras que ya tengo
-need = len(letras_t) # letras q necesito
-window = {}
-result = ""
-result_len = float("inf")
+need = len(letras_t) # letras q necesito 
+window = {} # diccionario de  las letrras q este en tu resultado
+resul = "" # mejor de los string
+resul_len = float("inf") 
 l = 0
-
 for r in range(len(s)):
-    char = s[r]
-    window[char] = window.get(char, 0) + 1
+    letra = s[r]
+    window[letra] = window.get(letra,0) + 1
 
-    if char in letras_t and window[char] == letras_t[char]:
+    if letra in letras_t and window[letra] == letras_t[letra]:
         have += 1
-
     while have == need:
-        if (r - l + 1) < result_len:
-            result_len = r - l + 1
-            result = s[l:r + 1]
-
+        if r - l + 1 < resul_len:
+            resul_len = r - l + 1
+            resul = s[l:r + 1]
+        
         window[s[l]] -= 1
         if s[l] in letras_t and window[s[l]] < letras_t[s[l]]:
             have -= 1
         l += 1
 
-print(result)
+print(resul)
