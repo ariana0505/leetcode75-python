e81e474ec95df0aa96af63dee66b52590a27a0d1
ariana0505 <penaariana075@gmail.com>
2026-03-31 23:00:24 -0500

    feat: add Group Anagrams practice using sorted tuple dictionary grouping

--- practice/group_anagrams.py
+++ practice/group_anagrams.py
@@ -1,10 +1,12 @@
-palabras = ["a"]
+palabras = ["eat","tea","tan","ate","nat","bat"]
+
 diccionario = {}
 
 for palabra in palabras:
-    letras = tuple(sorted(palabra)) 
+    letras = tuple(sorted(palabra))
     if letras in diccionario:
         diccionario[letras].append(palabra)
     else:
-        diccionario[letras] = [palabra]
+        diccionario[letras]  = [palabra]
+
 print(list(diccionario.values()))
