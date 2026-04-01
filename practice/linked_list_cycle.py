9c69398c44c9612de9c7c2719e577524085ea68d
ariana0505 <penaariana075@gmail.com>
2026-04-01 00:16:50 -0500

    feat: add Linked List Cycle practice using Floyd's two-pointer detection

--- practice/linked_list_cycle.py
+++ practice/linked_list_cycle.py
@@ -1,22 +1,25 @@
 class ListNode:
-    def __init__(self, valor = 0,next =None):
-        self.valor =  valor
+    def __init__(self, valor = 0, next = None):
+        self.valor = valor
         self.next = next
 
 node4 =  ListNode(3)
-node3 = ListNode(4,node4)
-node2 = ListNode(5, node3)
-node1 = ListNode(8, node2)
-node4.next = node3
+node3 =  ListNode(5)
+node2 = ListNode(6)
+node1 = ListNode(8)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2
 
 slow =  node1
 fast = node1
 
+
 while fast and fast.next:
     slow = slow.next
-    fast = fast.next.next
-
-    if slow  == fast:
+    fast  = fast.next.next
+    if slow == fast:
         print(True)
         break
 else:
