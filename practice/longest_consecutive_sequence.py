a1fd33ce20b435d434525d463fdce054a0e6c8e4
ariana0505 <penaariana075@gmail.com>
2026-04-03 00:38:49 -0500

    feat: add Longest Consecutive Sequence practice using set lookup

--- practice/longest_consecutive_sequence.py
+++ practice/longest_consecutive_sequence.py
@@ -1,13 +1,10 @@
 nums = [100,4,200,1,3,2]
 nums_set = set(nums)
-mayor = 0
-for num in nums_set:
+mayor_longitud =  0
+for num in nums:
     if num - 1 not in nums_set:
-        largo = 1
-        while num + largo in nums_set:
-            largo += 1
-        mayor = max(mayor,largo)
-
-print(mayor)
-
-
+        longitud = 1
+        while num + longitud in  nums_set:
+            longitud += 1
+        mayor_longitud = max(longitud,mayor_longitud)
+print(mayor_longitud)
