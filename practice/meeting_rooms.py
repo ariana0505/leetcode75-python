d238a94f273f20f3015e7a1fe36654822ad24413
ariana0505 <penaariana075@gmail.com>
2026-04-03 00:49:29 -0500

    feat: add Meeting Rooms practice using sorted interval overlap check

--- practice/meeting_rooms.py
+++ practice/meeting_rooms.py
@@ -1,6 +1,10 @@
 intervals = [[7,10],[2,4]]
+
+
 intervals.sort()
-for i in range(len(intervals)-1):
+
+
+for i in range(len(intervals) - 1):
     if intervals[i][1] > intervals[i + 1][0]:
         print(False)
         break
