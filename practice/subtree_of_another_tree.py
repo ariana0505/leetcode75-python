ecae8458f5011fdc3c2bcde1badd495d8c78eb84
ariana0505 <penaariana075@gmail.com>
2026-04-04 16:17:35 -0500

    feat: add Subtree of Another Tree practice using recursive isSame comparison

--- practice/subtree_of_another_tree.py
+++ practice/subtree_of_another_tree.py
@@ -1,32 +1,17 @@
-class TreeNode:
-    def __init__(self, valor ,izquierda= None, derecha=None):
+class Treenode:
+    def __init__(self,valor,izquierda = None,derecha= None):
         self.valor = valor
-        self.derecha = derecha
         self.izquierda = izquierda
+        self.derecha = derecha
 
-
-def is_same(a: TreeNode,b:TreeNode):
-    if not a and not b:
-        return True
-    if not a or not b:
+tree =  Treenode(4, Treenode(5,Treenode(6),Treenode(9)),Treenode(8,Treenode(7),Treenode(6)))
+subtree = Treenode(5,Treenode(6),Treenode(9))
+prueba = Treenode(5,Treenode(7),Treenode(9))
+def isSame(root,subroot):
+    if not root and not subroot:
+        return  True
+    if not root or not subroot or root.valor != subroot.valor:
         return False
-    return (a.valor == b.valor and
-            is_same(a.izquierda, b.izquierda) and
-            is_same(a.derecha, b.derecha))
+    return ( isSame(root.izquierda, subroot.izquierda) and  isSame(root.derecha ,  subroot.derecha))
 
-
-def is_subtree(tree, subtree):
-    if not tree:
-        return False
-    if is_same(tree,subtree):
-        return True
-    return (is_subtree(tree.izquierda, subtree) or is_subtree(tree.derecha, subtree))
-
-root1 = TreeNode(3, TreeNode(4, TreeNode(1), TreeNode(2)), TreeNode(5))
-sub1 = TreeNode(4, TreeNode(1), TreeNode(2))
-print(is_subtree(root1, sub1))  # True
-
-# Example 2: root = [3,4,5,1,2(->0),null,null], subRoot = [4,1,2] -> false
-root2 = TreeNode(3, TreeNode(4, TreeNode(1), TreeNode(2, TreeNode(0))), TreeNode(5))
-sub2 = TreeNode(4, TreeNode(1), TreeNode(2))
-print(is_subtree(root2, sub2))  # False
+print(isSame(subtree, prueba))
