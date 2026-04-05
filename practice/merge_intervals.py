18cad6c97cbbb4eab5b74d76953361197cb530c5
ariana0505 <penaariana075@gmail.com>
2026-04-04 22:34:59 -0500

    refactor: rewrite Merge Intervals practice with cleaner variable names

--- practice/merge_intervals.py
+++ practice/merge_intervals.py
@@ -1,13 +1,10 @@
+
 intervals = [[1,3],[2,6],[8,10],[15,18]]
-
 intervals.sort()
-resultado = [intervals[0]]
-
+resul = [intervals[0]]
 for i in range(1,len(intervals)):
-    if resultado[-1][1] >= intervals[i][0]:
-        resultado[-1][1] = max(resultado[-1][1], intervals[i][1])
+    if intervals[i][0] <= resul[-1][1]:
+        resul[-1][1] = max(intervals[i][1],resul[-1][1])
     else:
-        resultado.append(intervals[i])
-
-print(resultado)
-
+        resul.append(intervals[i])
+print(resul)
