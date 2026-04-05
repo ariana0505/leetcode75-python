334d7832026d9db168960df703d8d29bd5d4be29
ariana0505 <penaariana075@gmail.com>
2026-04-04 20:44:01 -0500

    feat: add House Robber II practice with circular array edge case handling

--- practice/house_robber2.py
+++ practice/house_robber2.py
@@ -1,16 +1,11 @@
 nums = [1,2,3,1]
-
 def rob(nums):
-    rob1 = 0
-    rob2 = 0
-
-    for num in nums:
-        temp = max(num + rob1,  rob2)
-        rob1 = rob2
+    rob1,  rob2 = 0,0
+    for num  in nums:
+        temp  = max(rob1 + num,rob2)
+        rob1   = rob2
         rob2 = temp
+    
     return rob2
 
-if len(nums) == 1:
-    print(nums[0])
-else:
-    print(max(rob(nums[:-1]),rob(nums[1:])))
+print(max(rob(nums[1:]),rob(nums[:-1])))
