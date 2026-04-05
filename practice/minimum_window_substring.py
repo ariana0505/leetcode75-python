72ec9e128880ecdc209e6637fcb8c47821fdde3d
ariana0505 <penaariana075@gmail.com>
2026-04-05 17:00:07 -0500

    fix: use distinct letter count in Minimum Window Substring practice to handle duplicates in t

--- practice/minimum_window_substring.py
+++ practice/minimum_window_substring.py
@@ -2,27 +2,26 @@ from collections import Counter
 s = "ADOBECODEBANC"
 t = "ABC"
 
-tengo  = 0
-necesito = Counter(t)
-faltan  = len(t)
-ventana =  {}
+ventana = {}
+necesito  = Counter(t)
+faltan = len(necesito)
 resul = ''
 resul_len = float('inf')
-
 l = 0
+
 for r in range(len(s)):
     letra = s[r]
     ventana[letra] = ventana.get(letra,0) + 1
-    if letra in necesito and necesito[letra] == ventana[letra]:
-        faltan  -= 1
+    if letra in necesito and ventana[letra] == necesito[letra]:
+        faltan -= 1
     while faltan == 0:
-        ventana_len  = r-l+ 1
+        ventana_len = r - l + 1
         if ventana_len < resul_len:
-            resul_len  = ventana_len
-            resul = s[l:r+1]
+            resul_len = ventana_len
+            resul = s[l:r +1]
         letra_l = s[l]
-        ventana[letra_l] -= 1
-        if letra_l  in necesito and ventana[letra_l] < necesito[letra_l]:
-            faltan +=1
-        l +=1
+        ventana[letra_l] -=  1 
+        if letra_l in necesito and  ventana[letra_l] <  necesito[letra_l]:
+            faltan += 1
+        l += 1
 print(resul)
