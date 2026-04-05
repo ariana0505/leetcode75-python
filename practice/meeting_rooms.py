ae5fb2a00bd0f8316b19bcefad8707b1c8298f4c
ariana0505 <penaariana075@gmail.com>
2026-04-04 22:10:15 -0500

    fix: add break to Meeting Rooms practice to stop on first overlap

--- practice/meeting_rooms.py
+++ practice/meeting_rooms.py
@@ -1,11 +1,8 @@
-intervals = [[7,10],[2,4]]
 
-
+intervals = [[0,30],[5,10],[15,20]]
 intervals.sort()
-
-
-for i in range(len(intervals) - 1):
-    if intervals[i][1] > intervals[i + 1][0]:
+for i in range(len(intervals)-1):
+    if  intervals[i][1] > intervals[i + 1][0]:
         print(False)
         break
 else:
