bcd66fe53e2ad9022069bfe9de1c8aa52ef2eaeb
ariana0505 <penaariana075@gmail.com>
2026-04-13 17:54:25 -0500

    feat: add Climbing Stairs practice solution

--- practice/climbing_stairs.py
+++ practice/climbing_stairs.py
@@ -1,11 +1,8 @@
-n = 5
+n = 2
 num1 = 1
 num2 = 1
-for i in range(n- 1):
-    temp = num1 + num2
+for i  in range(n):
+    temp = max(num1 + i ,num2)
     num1 = num2
     num2 = temp
-
-    
-
 print(num2)
