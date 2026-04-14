a8e9fd8f21181184a4d25345a2f1270fb14cd956
ariana0505 <penaariana075@gmail.com>
2026-04-13 21:20:47 -0500

    feat: add Find Minimum in Rotated Sorted Array practice solution

--- practice/find-minimum-in-rotated-sorted-array.py
+++ practice/find-minimum-in-rotated-sorted-array.py
@@ -1,14 +1,13 @@
-nums = [3,4,5]
-resul = nums[0]
+nums = [3,4,5,1,2]
+
 l = 0
 r = len(nums) - 1
-while l <= r:
-    mid =(l +r)// 2
-    resul = min (nums[mid],resul )
-    if nums[l]  <= nums[r]:
-        print(nums[l])
-        break
-    if nums[mid] >= nums[l]:
-        l = mid  + 1
+
+while l<r:
+
+    mid = (l + r )//2
+    if nums[mid] < nums[r]:
+        l = mid + 1
     else:
-        r = mid - 1
+        r = mid
+print(nums[mid])
