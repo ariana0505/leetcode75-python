791ce7bcf6974adfc0478a6827cad79a088e784d
ariana0505 <penaariana075@gmail.com>
2026-04-13 22:06:24 -0500

    feat: add Jump Game practice solution

--- practice/jump_game.py
+++ practice/jump_game.py
@@ -1,10 +1,7 @@
-nums = [3,2,1,1,4]   
-meta = len(nums)  - 1
-for i in range(len(nums) -2,-1,-1):
+nums = [2,3,1,0,4]
+meta = len(nums)- 1
+for i in range(len(nums) - 2, -1, -1):
     if nums[i] + i >= meta:
         meta = i
-
-if meta == 0:
-    print(True)
-else:
-    print(False)
+    
+print(meta == 0)
