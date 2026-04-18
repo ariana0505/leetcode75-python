1ac963f00e30c1f81f23666fd1f3cf8400724e94
ariana0505 <penaariana075@gmail.com>
2026-04-17 23:29:18 -0500

    feat: add Missing Number practice solution

--- practice/missing_number.py
+++ practice/missing_number.py
@@ -1,6 +1,6 @@
-nums = [9, 6, 4, 2, 3, 5, 7, 0, 1]
+nums = [9,6,4,2,3,5,7,0,1]
 n = len(nums)
-suma_esperada = n * (n + 1) // 2
-suma_verdadera = sum(nums)
-
-print(suma_esperada -  suma_verdadera)
+print(n)
+suma_nums = sum(nums)
+suma_esperada =(n * (n + 1))/2
+print(int(suma_esperada - suma_nums))
