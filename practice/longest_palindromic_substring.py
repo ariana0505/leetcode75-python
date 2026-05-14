a9dc233194104da4a29345ae9e13eb1947c01139
ariana0505 <penaariana075@gmail.com>
2026-05-14 14:56:09 -0500

    feat: practice Longest Palindromic Substring using expand around center

---
+++ practice/longest_palindromic_substring.py
@@ -0,0 +1,24 @@
+s =  "babab"
+
+resol= ""
+resol_t = 0
+
+for i in range(len(s)):
+    l,r=i,i
+    # impares
+    while l >= 0 and r < len(s) and s[l]  == s[r]:
+        if r - l + 1  > resol_t:
+            resol = s[l :  r +1]
+            resol_t = r - l + 1
+        l-=1
+        r+=1
+    # pares
+    l,r=i,i+1
+    while l >= 0 and r < len(s) and s[l]  == s[r]:
+        if r - l + 1  > resol_t:
+            resol = s[l :  r +1]
+            resol_t = r - l + 1
+        l-=1
+        r+=1
+
+print(resol)
