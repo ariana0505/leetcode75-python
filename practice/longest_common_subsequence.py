cad14a27a87a11c53f6c6cc324f5817ac4dda64c
ariana0505 <penaariana075@gmail.com>
2026-07-05 08:06:56 -0500

    solve Longest Common Subsequence with memoization in practice

--- practice/longest_common_subsequence.py
+++ practice/longest_common_subsequence.py
@@ -1,18 +1,17 @@
-from  functools import lru_cache
+from functools  import  lru_cache
 
-text1="abcde"
-text2="ace"
+text1 = "abcde"
+text2 = "ace"
 
-i,j  =  0,0
-
+i,j = 0,0
 @lru_cache
-def lcs(i,j):
-    
-    if i == len(text1) or j == len(text2):
+def lcs(i, j):
+    if i == len(text1)  or j ==  len(text2):
         return 0
-    if text1[i]  == text2[j]:
-        return 1 + lcs(i+1,j+1)
+    
+    if text1[i] ==  text2[j]:
+        return 1 +  lcs(i+1,j+1)
     else:
-        return max(lcs(i+1,j),  lcs(i,j+1))
-
+        return max(lcs(i+1,j),lcs(i,j+1))
+    
 print(lcs(0,0))
