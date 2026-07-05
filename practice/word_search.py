11ceaef2bda7a4930a390f3fb6bbae07e3ab3750
ariana0505 <penaariana075@gmail.com>
2026-07-05 11:57:22 -0500

    solve word search with DFS backtracking in practice

--- practice/word_search.py
+++ practice/word_search.py
@@ -1,31 +1,32 @@
 board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
-word = "SEE"
+word = "ABCB"
 
-filas = len(board) 
-columnas = len(board[0]) 
+filas  = len(board)
+columnas  = len(board[0])
 
 def dfs(r,c,k):
-    if k == len(word):
+    if  k  ==  len(word):
         return True
-    if r < 0 or r>= filas or c< 0 or c>= columnas:
-        return False  
+    if  r < 0 or r >= filas or c  <  0 or c  >= columnas:
+        return  False
     if word[k] != board[r][c]:
         return False
-    if board[r][c]  == "#":
-        return False
-    temp  = board[r][c]
-    board[r][c]  = "#"
-    encontrado = (dfs(r+1,c,k+1)  or dfs(r-1,c,k+1) or  dfs(r,c+1,k+1)  or  dfs(r,c-1,k+1))
+    if board[r][c]  ==  "#":
+        return  False
+    temp =  board[r][c]
+    board[r][c] =  "#"
+    encontrado =  (dfs(r+1,c,k+1) or dfs(r - 1,c,k+1) or dfs(r,c+1,k+1) or dfs(r,c-1,k+1))
     board[r][c] = temp
     return encontrado
 
-encontrado =  False
+visto  =   False
 
-for r in range(filas):
-    for  c in  range(columnas):
-        if dfs(r,c,0):
-            encontrado = True
+for r  in  range(filas):
+    for c  in range(columnas):
+        if  dfs(r,c,0):
+            visto = True
             break
-    if  encontrado:
+    if visto:
         break
-print(encontrado)
+
+print(visto)
