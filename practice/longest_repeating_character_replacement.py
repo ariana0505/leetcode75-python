29e7e66a7bd6f92625e416c9c831070b331dfde5
ariana0505 <penaariana075@gmail.com>
2026-07-05 18:37:55 -0500

    test longest repeating character replacement solution in practice

--- practice/longest_repeating_character_replacement.py
+++ practice/longest_repeating_character_replacement.py
@@ -1,13 +1,15 @@
-s = "ABAB"
+s = "ABABB"
 k = 1
-counter =  {}
+
+contador  = {}
+maximo = 0
 l = 0
-maximo =  0
-for r in  range(len(s)):
-    counter[s[r]] = counter.get(s[r],0) + 1
-    if (r -  l  +  1)  - max(counter.values()) > k:
-        counter[s[l]] -= 1
+
+for  r in  range(len(s)):
+    contador[s[r]] = contador.get(s[r],0)+1
+    if (r-l + 1) - max(contador.values()) > k:
+        contador[s[l]] -= 1
         l += 1
-    maximo = max(maximo, r-l+1)
     
+    maximo  =  max(maximo,  r-l+1)
 print(maximo)
