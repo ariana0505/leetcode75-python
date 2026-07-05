a98e64aa693109b2c7755549169a733c949f2880
ariana0505 <penaariana075@gmail.com>
2026-07-05 09:34:27 -0500

    solve Valid Palindrome with two pointers in practice

--- practice/valid_palindrome.py
+++ practice/valid_palindrome.py
@@ -1,19 +1,16 @@
-s  =  "A man, a plan, a canal: Panama"
+s = "race a car"
+l =  0
+r =  len(s) - 1
 
-r,l = 0, len(s) -  1
-
-while  r < l:
-     # saltar basura por la izquierda
-    while r < l and not s[r].isalnum():
-        r += 1
-    # saltar basura por la derecha
-    while r < l and not s[l].isalnum():
-        l -= 1
-    # aquí s[r] y s[l] YA son válidos 
-
-    if s[r].lower()  ==  s[l].lower():
-        r+=1
-        l -=1
+while l<r:
+    while l<r and not s[l].isalnum():
+        l += 1
+    while l<r and not s[r].isalnum():
+        r -= 1
+    
+    if s[l].lower() ==  s[r].lower():
+        l +=1
+        r  -=  1
     else:
         print(False)
         break
