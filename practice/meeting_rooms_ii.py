c801a2b15dbac31a174507736386cbc2e06b3bce
ariana0505 <penaariana075@gmail.com>
2026-07-05 18:21:58 -0500

    test meeting rooms ii solution in practice

--- practice/meeting_rooms_ii.py
+++ practice/meeting_rooms_ii.py
@@ -1,20 +1,20 @@
-intervals = [[0,30],[5,10],[15,20]]
+intervals = [[7,10],[2,4]]
 
-inicios = sorted([interval[0]  for interval  in intervals])
-finales = sorted([interval[1]  for interval  in intervals])
+inicios = sorted([interval[0]  for  interval in intervals])
+finales = sorted([interval[1]  for  interval in intervals])
 
-salas_ahora = 0
-salas_maximas = 0
+salas_ahora =  0
+salas_maximo  = 0
 
-i,f =   0,0
+i,f =0,0
 
-while  i < len(intervals):
+while i < len(intervals):
     if inicios[i] < finales[f]:
-        i += 1
         salas_ahora += 1
-        salas_maximas   =  max(salas_maximas,salas_ahora)
+        i += 1
+        salas_maximo = max(salas_maximo,salas_ahora)
     else:
         f += 1
-        salas_ahora -= 1
+        salas_ahora -=  1
 
-print(salas_maximas)
+print(salas_maximo)
