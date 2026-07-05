8554565b22e3310de1e014f3878e84ed604ae703
ariana0505 <penaariana075@gmail.com>
2026-07-05 10:18:21 -0500

    solve Word Search with DFS backtracking in practice

--- practice/word_search.py
+++ practice/word_search.py
@@ -1,42 +1,31 @@
-board = [
-    ["A", "B", "C", "E"],
-    ["S", "F", "C", "S"],
-    ["A", "D", "E", "E"],
-]
-word = "ABCCED"
+board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]
+word = "SEE"
 
-filas = len(board)
-columnas = len(board[0])
+filas = len(board) 
+columnas = len(board[0]) 
 
-
-def dfs(r, c, k):
-    if len(word) == k:
+def dfs(r,c,k):
+    if k == len(word):
         return True
-    if r < 0 or r >= filas or c < 0 or c >= columnas:
+    if r < 0 or r>= filas or c< 0 or c>= columnas:
+        return False  
+    if word[k] != board[r][c]:
         return False
-    if board[r][c] == "#":
+    if board[r][c]  == "#":
         return False
-    if board[r][c] != word[k]:
-        return False
-
-    temp = board[r][c]
-    board[r][c] = "#"
-
-    encontrado = (dfs(r + 1, c, k + 1) or dfs(r - 1, c, k + 1) or dfs(r, c + 1, k + 1) or dfs(r, c - 1, k + 1))
-
+    temp  = board[r][c]
+    board[r][c]  = "#"
+    encontrado = (dfs(r+1,c,k+1)  or dfs(r-1,c,k+1) or  dfs(r,c+1,k+1)  or  dfs(r,c-1,k+1))
     board[r][c] = temp
-
     return encontrado
 
-
-resultado = False
+encontrado =  False
 
 for r in range(filas):
-    for c in range(columnas):
-        if dfs(r, c, 0):
-            resultado = True
+    for  c in  range(columnas):
+        if dfs(r,c,0):
+            encontrado = True
             break
-    if resultado:
+    if  encontrado:
         break
-
-print(resultado)
+print(encontrado)
