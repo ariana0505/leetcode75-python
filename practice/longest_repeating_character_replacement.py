f4a206aaab31b4de7997bac92ce9bf1decc3cf19
ariana0505 <penaariana075@gmail.com>
2026-07-05 08:34:25 -0500

    solve Longest Repeating Character Replacement with sliding window in practice

--- practice/longest_repeating_character_replacement.py
+++ practice/longest_repeating_character_replacement.py
@@ -1,13 +1,14 @@
 s = "AABABBA"
 k = 1
-contador = {}
-resul = 0
-l = 0
-for r in range(len(s)):
-    contador[s[r]] =contador.get(s[r],0) +  1
-    while  (r - l + 1 ) - max(contador.values()) > k:
-        contador[s[l]] -= 1
-        l +=  1
-    resul = max(resul,  r - l +  1)
 
-print(resul)
+contador  =   {}
+maximo  = 0
+l  = 0
+for r in range(len(s)):
+    contador[s[r]] = contador.get(s[r],0) + 1
+    if (r-l +1) - max(contador.values()) > k:
+        contador[s[l]]  -= 1
+        l+=1
+    maximo = max(maximo,r-l +1)
+
+print(maximo)
