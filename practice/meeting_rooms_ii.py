f93ad5812629a8804c0163f56f9f0b413fd10e20
ariana0505 <penaariana075@gmail.com>
2026-07-05 12:09:17 -0500

    solve meeting rooms ii with two pointers in practice

--- practice/meeting_rooms_ii.py
+++ practice/meeting_rooms_ii.py
@@ -1,21 +1,20 @@
 intervals = [[0,30],[5,10],[15,20]]
 
-inicios= sorted([interval[0] for interval in intervals])
-finales= sorted([interval[1] for interval in intervals])
+inicios = sorted([interval[0]  for interval  in intervals])
+finales = sorted([interval[1]  for interval  in intervals])
 
-i,j = 0,0
-
-salas_maximas =  0
 salas_ahora = 0
+salas_maximas = 0
 
-while  i  <  len(intervals):
-    if  inicios[i] <  finales[j]:
-        salas_ahora  +=  1
-        i  += 1
-        salas_maximas =  max(salas_ahora,salas_maximas)
+i,f =   0,0
+
+while  i < len(intervals):
+    if inicios[i] < finales[f]:
+        i += 1
+        salas_ahora += 1
+        salas_maximas   =  max(salas_maximas,salas_ahora)
     else:
+        f += 1
         salas_ahora -= 1
-        j += 1
-
 
 print(salas_maximas)
