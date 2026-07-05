58395ca114f7fdc32e703bbc45c57ce98142d96a
ariana0505 <penaariana075@gmail.com>
2026-07-05 12:18:04 -0500

    solve decode ways with dynamic programming in practice

--- practice/decode_ways.py
+++ practice/decode_ways.py
@@ -1,16 +1,11 @@
 s = "226"
-
-dp = [0] * (len(s) +  1)
+dp =  [0]  * (len(s) +  1)
 dp[0] = 1
-dp[1] =  1 if s[0] != "0" else 0
+dp[1]  = 1 if s[0] != "0" else 0
 
-for  i  in range(2,len(s) + 1):
-
-    if 0  < int(s[i-1])  <=  9:
-        dp[i] = dp[i-1] +  dp[i]
-
-    if 10 <= int(s[i-2:i]) <=  26:
-        dp[i] = dp[i-2] + dp[i]
-        
-        
+for i in range(2,len(s)+1):
+    if 0 < int(s[i-1]) <=9:
+        dp[i] =  dp[i - 1]  + dp[i]
+    if 10 <= int(s[i-2:i]) <=26:
+        dp[i] = dp[i - 2]  + dp[i]
 print(dp[len(s)])
