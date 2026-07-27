dbd287cd193d9ba67e4e123c3930e782f6bddb30
ariana0505 <penaariana075@gmail.com>
2026-07-27 13:06:21 -0500

    refactor: use PEP 585 built-in generics in 10-conteiner-most_water/conteiner_most_water.py

--- 10-conteiner-most_water/conteiner_most_water.py
+++ 10-conteiner-most_water/conteiner_most_water.py
@@ -1,7 +1,6 @@
-from typing import List
 
 # Two-pointer approach to find the container that holds the most water
-height: List[int] = [1, 3, 7, 5, 3, 2, 6, 9, 1]
+height: list[int] = [1, 3, 7, 5, 3, 2, 6, 9, 1]
 max_area: int = 0
 l, r = 0, len(height) - 1
 
