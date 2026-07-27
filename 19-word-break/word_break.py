6b780580c2f2d1e2b87973ff23efb75b27476a23
ariana0505 <penaariana075@gmail.com>
2026-07-27 13:06:29 -0500

    refactor: use PEP 585 built-in generics in 19-word-break/word_break.py

--- 19-word-break/word_break.py
+++ 19-word-break/word_break.py
@@ -1,10 +1,9 @@
-from typing import List
 
 # Determine whether s can be segmented into words from the dictionary
 s: str = "leetcode"
-wordDict: List[str] = ["leet", "code"]
+wordDict: list[str] = ["leet", "code"]
 
-dp: List[bool] = [False] * (len(s) + 1)
+dp: list[bool] = [False] * (len(s) + 1)
 dp[len(s)] = True
 
 for i in range(len(s) - 1, -1, -1):
