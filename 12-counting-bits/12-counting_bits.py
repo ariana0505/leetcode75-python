7c48799f1ca3764e11c4b562300c3d7a1928c0b4
ariana0505 <penaariana075@gmail.com>
2026-07-27 13:06:23 -0500

    refactor: use PEP 585 built-in generics in 12-counting-bits/12-counting_bits.py

--- 12-counting-bits/12-counting_bits.py
+++ 12-counting-bits/12-counting_bits.py
@@ -1,8 +1,7 @@
-from typing import List
 
 # Count the number of 1-bits for every number from 0 to n
 n: int = 5
-dp: List[int] = [0] * (n + 1)
+dp: list[int] = [0] * (n + 1)
 offset: int = 1
 for i in range(1, n + 1):
     if offset * 2 == i:
