def8cd0c617152a7a4e4c40de7d8ff37f233f9b2
ariana0505 <penaariana075@gmail.com>
2026-08-04 15:56:34 -0500

    practice: add two-heap solution for find median from data stream

--- practice/find_median_from_data_stream.py
+++ practice/find_median_from_data_stream.py
@@ -1,22 +1,16 @@
 import heapq
 class MedianFinder:
     def __init__(self):
-        self.baja = []
-        self.alta = []
+        self.bajo = []
+        self.alto = []
 
+    def addNum(self, nodo):
+        heapq.heappush(self.bajo, -nodo)
+        heapq.heappush(self.alto, -heapq.heappop(self.bajo))
+        if len(self.bajo)< len(self.alto):
+            heapq.heappush(self.bajo,-heapq.heappop(self.alto))
     def findMedian(self):
-        if len(self.baja) > len(self.alta):
-            return -self.baja[0]
+        if len(self.bajo) > len(self.alto):
+            return -self.bajo[0]
         else:
-            return (-self.baja[0] + self.alta[0]) / 2
-    def addNum(self,num):
-        heapq.heappush(self.baja, -num)
-        heapq.heappush(self.alta, -heapq.heappop(self.baja))
-        if len(self.baja) < len(self.alta):
-            heapq.heappush(self.baja, -heapq.heappop(self.alta))
-
-calculo = MedianFinder()
-calculo.addNum(2)
-calculo.addNum(5)
-calculo.addNum(8)
-print(calculo.findMedian())
+            return (-self.bajo[0] + self.alto[0]) / 2
