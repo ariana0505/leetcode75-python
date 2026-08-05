fc46fe24f80ce6c8958a9b20946da9d89cea869d
ariana0505 <penaariana075@gmail.com>
2026-08-05 01:14:11 -0500

    practice: clear clone_graph.py

---
+++ 46-spiral-matrix/spiral_matrix.py
@@ -0,0 +1,22 @@
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+
+result = []
+
+izq , der = 0 , len(matrix[0]) - 1
+
+top , bottom = 0, len(matrix) - 1
+
+while top <= bottom and izq <= der:
+    # top representa la columna que a de recorrer
+    # sus topes es izq y der + 1
+    for col in range(izq,der +1):
+        result.append(matrix[top][col])
+    top += 1
+
+    # fila representa bajar de top a bottom
+    for fila in range(top, bottom + 1):
+        result.append(matrix[fila][der])
+
+    der -= 1
+
+# FALTA RESOLVER
--- practice/clone_graph.py
+++ practice/clone_graph.py
@@ -1,20 +1,2 @@
-class Node:
-    def __init__(self,val,vecinos = None):
-        self.val = val
-        self.vecinos = vecinos if vecinos is not None else []
 
-visitados = {} # nodo : nodo copia
-def clone_graph(nodo : Node):
-    if not nodo:
-        return None
-    if nodo in visitados:
-        return visitados[nodo]
-    visitados[nodo] = Node(nodo.val) # -> nodo : nodo copia (SIN VECINOS)
-    copi = visitados[nodo]
 
-    #OBTENEMOS VECINOS
-
-    for vecino in nodo.vecinos:
-        copi.vecinos.append(clone_graph(vecino))
-
-    return copi
