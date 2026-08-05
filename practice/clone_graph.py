b22235d6a14ddd7ede5dbdcab788e3dca3b967f5
ariana0505 <penaariana075@gmail.com>
2026-08-05 16:59:07 -0500

    feat(clone-graph): add DFS solution with memoization
    Uses a visited dict to handle cycles and avoid infinite recursion

--- practice/clone_graph.py
+++ practice/clone_graph.py
@@ -1,2 +1,21 @@
+class Nodo:
+    def __init__(self, val, vecinos = None):
+        self.val = val
+        self.vecinos = vecinos if vecinos is not  None else []
 
+visitados = {} #nodo - > nodo copiado
 
+def clone(nodo:Nodo):
+
+    if nodo == None:
+        return 
+    if nodo in visitados:
+        return visitados[nodo]
+    
+    copia = Nodo(nodo.val)
+    visitados[nodo] = copia
+
+    for vecino in nodo.vecinos:
+        copia.vecinos.append(clone(vecino))
+
+    return copia
