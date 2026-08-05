5336b5c4658883836fb10c6c005b7bbfc3ee03a4
ariana0505 <penaariana075@gmail.com>
2026-08-05 17:57:25 -0500

    feat(number-of-connected-components): implement DFS component count

--- practice/number_of_connected_components.py
+++ practice/number_of_connected_components.py
@@ -1,23 +1,28 @@
 n = 5
-edges = [[0,1],[1,2],[2,3],[3,4]]
-lista_ady = [[] for _ in range(n)]
+edges = [[0,1],[1,2],[3,4]]
+
+lista_ady = [ [] for _ in range(n) ]
 
 for a,b in edges:
     lista_ady[a].append(b)
     lista_ady[b].append(a)
 
-visitados = set()
+visitados = []
 
 def connected(nodo):
-    if nodo in visitados:
+    if nodo in visitados :
         return
-    visitados.add(nodo)
+
+    visitados.append(nodo)
+
     for vecino in lista_ady[nodo]:
         connected(vecino)
-componente = 0
+
+conenxos = 0 
+
 for i in range(n):
     if i not in visitados:
         connected(i)
-        componente += 1
+        conenxos += 1
 
-print(componente)
+print(conenxos)
