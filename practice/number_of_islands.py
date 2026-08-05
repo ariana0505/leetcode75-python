e4141d9db66792c0fe9c07d9fd19f323cb8fc989
ariana0505 <penaariana075@gmail.com>
2026-08-05 02:17:35 -0500

    practice: add DFS solution for number of islands

--- practice/number_of_islands.py
+++ practice/number_of_islands.py
@@ -1,29 +1,26 @@
 grid = [
+  ["1","1","1","1","0"],
+  ["1","1","0","1","0"],
   ["1","1","0","0","0"],
-  ["1","1","0","0","0"],
-  ["0","0","1","0","0"],
-  ["0","0","0","1","1"]
+  ["0","0","0","0","0"]
 ]
+filas = len(grid) 
+columnas = len(grid[0]) 
+islas = 0
 
-filas = len(grid)
-columnas = len(grid[0])
-contador = 0
-
-def count(fila,columna):
-    if 0> fila or 0> columna or columna >= columnas or fila >= filas or grid[fila][columna] != "1":
+def number_of_island(fila,columna):
+    if fila < 0 or columna < 0 or fila >= filas or columna >= columnas or grid[fila][columna] != "1":
         return
-
     grid[fila][columna] = "0"
-
-    count(fila + 1 , columna)
-    count(fila - 1 , columna)
-    count(fila , columna + 1)
-    count(fila , columna - 1)
+    number_of_island(fila-1,columna)
+    number_of_island(fila+1,columna)
+    number_of_island(fila,columna-1)
+    number_of_island(fila,columna+1)
 
 for fila in range(filas):
     for columna in range(columnas):
         if grid[fila][columna] == "1":
-            count(fila,columna)
-            contador += 1
+            islas += 1
+            number_of_island(fila,columna)
 
-print(contador)
+print(islas)
