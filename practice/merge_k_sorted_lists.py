a1e227294da6b8e802d3688b9427d3f127145042
ariana0505 <penaariana075@gmail.com>
2026-08-05 18:07:14 -0500

    feat(merge-k-sorted-lists): implement heap-based merge solution

--- practice/merge_k_sorted_lists.py
+++ practice/merge_k_sorted_lists.py
@@ -1,31 +1,35 @@
 import heapq
+
+
 class ListNode:
-    def __init__(self,val=0, next = None):
+    def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
 
-lista1 = ListNode(1,ListNode(4,ListNode(5)))
-lista2 = ListNode(1,ListNode(3,ListNode(4)))
-lista3 = ListNode(2,ListNode(6))
 
-listas = [lista1,lista2,lista3]
+list1 = ListNode(1, ListNode(4, ListNode(5)))
+list2 = ListNode(1, ListNode(3, ListNode(4)))
+list3 = ListNode(2, ListNode(6))
 
+listas = [list1, list2, list3]
 heap = []
-dummy = ListNode()
-tail = dummy
+
 for i,lista in enumerate(listas):
     if lista:
-        heapq.heappush(heap , (lista.val, i,lista))
+        heapq.heappush(heap,(lista.val,i, lista))
+
+dummy = ListNode()
+tail = dummy
 
 while heap:
     extraido = heapq.heappop(heap)
-    val, i , nodo = extraido
+    val , i , nodo = extraido
+    if nodo.next:
+        heapq.heappush(heap,(nodo.next.val, i, nodo.next))
     tail.next = nodo
     tail = tail.next
-    if nodo.next:
-        heapq.heappush(heap, (nodo.next.val, i, nodo.next))
 
 respuesta = dummy.next
 while respuesta:
-    print(respuesta.val,end="->")
-    respuesta= respuesta.next
+    print(respuesta.val, end="->")
+    respuesta = respuesta.next
