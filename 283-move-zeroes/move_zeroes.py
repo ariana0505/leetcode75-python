b2c11c67229535daf3a18384c2d6c488489ad68d
ariana0505 <penaariana075@gmail.com>
2026-08-06 01:07:39 -0500

    refactor(move-zeroes): use extra list to preserve order
    Simpler approach than two-pointer in-place, O(n) space instead of O(1)

--- 283-move-zeroes/move_zeroes.py
+++ 283-move-zeroes/move_zeroes.py
@@ -1,45 +1,10 @@
-from typing import List
+nums = [3, 0, 1, 0, 2]
+ceros = 0
+respuesta = []
+for num in nums:
+    if num == 0:
+        ceros += 1
+    else:
+        respuesta.append(num)
 
-
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        # Two pointers: insert_pos marks where the next non-zero goes
-        insert_pos = 0
-        for num in nums:
-            if num != 0:
-                nums[insert_pos] = num
-                insert_pos += 1
-        for i in range(insert_pos, len(nums)):
-            nums[i] = 0
-
-
-if __name__ == "__main__":
-    sol = Solution()
-
-    nums1 = [0, 1, 0, 3, 12]
-    sol.moveZeroes(nums1)
-    print(nums1)
-
-    nums2 = [0]
-    sol.moveZeroes(nums2)
-    print(nums2)
-
-    nums3 = [1, 2, 3]
-    sol.moveZeroes(nums3)
-    print(nums3)
-
-    nums4 = [0, 0, 0, 1]
-    sol.moveZeroes(nums4)
-    print(nums4)
-
-    nums5 = [4, 0, 5, 0, 6]
-    sol.moveZeroes(nums5)
-    print(nums5)
-
-    nums6 = [1]
-    sol.moveZeroes(nums6)
-    print(nums6)
-
-    nums7 = [0, 0]
-    sol.moveZeroes(nums7)
-    print(nums7)
+print(respuesta + [0] * ceros)
