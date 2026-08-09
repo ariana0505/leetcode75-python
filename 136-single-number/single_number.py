472e09aa94e8cf1d7c7de78e6dfa691fdb6e96df
ariana0505 <penaariana075@gmail.com>
2026-08-09 00:37:19 -0500

    refactor(single-number): use xor instead of counting dictionary
    O(1) space: duplicate values cancel out since x ^ x == 0

--- 136-single-number/single_number.py
+++ 136-single-number/single_number.py
@@ -1,9 +1,6 @@
 nums = [4,1,2,1,2]
-diccionario = {}
-for numero in nums:
-    diccionario[numero] = diccionario.get(numero, 0) + 1
-
-for numero, conteo in diccionario.items():
-    if conteo == 1:
-        print(numero)
-        break
+unico = 0
+for n in nums:
+    unico ^= n
+    
+print(unico)
