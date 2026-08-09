efb80b44e689e3cbd7adf8b5e0d303d58f0cda6e
ariana0505 <penaariana075@gmail.com>
2026-08-09 01:09:59 -0500

    refactor(binary-search): invert comparison and split bounds init
    same behavior, condition rewritten as nums[mid] < target

--- 704-binary-search/binary_search.py
+++ 704-binary-search/binary_search.py
@@ -1,15 +1,15 @@
 nums = [-1,0,3,5,9,12]
-target = 3
-
-izq, der = 0 , len(nums) - 1
+target = 2
+izq = 0
+der = len(nums) - 1
 while izq <= der:
     mid = (izq + der) // 2
     if nums[mid] == target:
         print(mid)
         break
-    if nums[mid]> target:
-        der = mid - 1
-    else:
+    if nums[mid] < target:
         izq = mid + 1
+    else:
+        der = mid - 1
 else:
     print(-1)
