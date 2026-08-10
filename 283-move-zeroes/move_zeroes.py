a749d71dd5611333615a4787ad72bd9f3204c252
ariana0505 <penaariana075@gmail.com>
2026-08-10 15:01:43 -0500

    chore: clear move-zeroes and binary-search solutions for re-practice

--- 283-move-zeroes/move_zeroes.py
+++
@@ -1,7 +0,0 @@
-nums = [0, 1, 0, 3, 12]
-izq = 0  # siguiente posicion donde colocar un no-cero
-for der in range(len(nums)):
-    if nums[der] != 0:
-        nums[izq],nums[der] = nums[der], nums[izq]
-        izq += 1
-print(nums)
